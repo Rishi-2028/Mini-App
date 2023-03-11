5555eada8e13cb220fb3443a9858edbac4ca1241
Rishi-2028 <rishibhandari2028@gmail.com>
2023-03-11 10:59:38 +0545

    Initial Commit

---
+++ MiniApp/MiniAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniAppApp.swift
+//  MiniApp
+//
+//  Created by Rishi on 11/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MiniAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
